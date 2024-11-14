3dee5d2aa9913267f21f85e50a4d6ad34c9d4b5a
julpermaz <julpermaz@alu.edu.gva.es>
2024-11-14 12:21:23 +0100

    ej2

---
+++ src/main/Ej_arrays/Ejemplo2_array.java
@@ -0,0 +1,29 @@
+package Ej_arrays;
+
+public class Ejemplo2_array {
+    public static void main(String[] args) {
+        int[] anArray;
+
+        // allocates memory for 10 integers
+        anArray = new int[10];
+
+        // initialize first element
+        anArray[0] = 100;
+        // initialize second element
+        anArray[1] = 200;
+        // and so forth
+        anArray[2] = 300;
+        anArray[3] = 400;
+        anArray[4] = 500;
+        anArray[5] = 600;
+        anArray[6] = 700;
+        anArray[7] = 800;
+        anArray[8] = 900;
+        anArray[9] = 1000;
+
+        for (int i = anArray.length -1; i >=0 ; i--) {
+            System.out.println(anArray[i]);
+        }
+
+    }
+}
